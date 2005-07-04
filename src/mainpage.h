92c1e137fb840c6701dcaa599b0b54fc5b630e61
burnett <>
2005-07-04 00:50:16 +0000

    unix fix, documentation

---
+++ src/mainpage.h
@@ -0,0 +1,12 @@
+//$Header: /nfs/slac/g/glast/ground/cvs/classifier/src/mainpage.h,v 1.1.1.1 2005/07/03 21:31:35 burnett Exp $
+// (Special "header" just for doxygen)
+
+/*! @mainpage  package classifier
+
+This package contains classes to manage classification 
+
+
+  <hr>
+
+
+*/
