b82706adcb547c908616c96ba09869abd1c5b76b
usher <>
2005-12-08 20:40:32 +0000

    Add "the kitchen sink" .... set up to write all "local" variables to ntuple from the current (V2r1) Insightful Miner analysis. This for DC-2 reprocessing. Future plan is to have a scheme for naming variables in Insightful Miner and then to automate this process...

--- GlastClassify/AtwoodTrees.h
+++ GlastClassify/AtwoodTrees.h
@@ -1,7 +1,7 @@
 /** @file AtwoodTrees.h
     @brief  Declare class AtwoodTrees
 
-$Header: /nfs/slac/g/glast/ground/cvs/GlastClassify/GlastClassify/AtwoodTrees.h,v 1.3 2005/11/22 21:19:01 usher Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/GlastClassify/GlastClassify/AtwoodTrees.h,v 1.4 2005/11/22 21:36:54 usher Exp $
 */
 #ifndef GlastClassify_AtwoodTrees_h
 #define GlastClassify_AtwoodTrees_h
@@ -40,21 +40,36 @@ public:
 
 private:
 
+    // These are variables used by the code
     const Item*   m_TkrNumTracks;
     const Item*   m_CalEnergyRaw  ;
     const Item*   m_CalCsIRLn   ;  
     const Item*   m_EvtEventId;
 
+    // These are variables to be output to the ntuple 
+    // (in alphabetical order)
+    float         m_acdLowerTileCount;
+    float         m_acdUpperTileCount;
+    float         m_bestPsfErr;
+    float         m_bestXDir;
+    float         m_bestYDir;
+    float         m_bestZDir;
+    float         m_bestDeltaEoE;
+    float         m_bestEnergy;
     float         m_bestEnergyProb;
-    float         m_profileProb;
-    float         m_lastLayerProb;
-    float         m_trackerProb;
-    float         m_paramProb;
-    float         m_CTBestEnergy;
-    float         m_CTBdeltaEoE;
-    float         m_VTX;
     float         m_CORE;
+    float         m_calFrontBackRatio;
+    float         m_calMaxXtalRatio;
+    float         m_evtLogEnergyRaw;
     float         m_GAM;
+    float         m_goodEnergy;
+    float         m_lastLayerProb;
+    float         m_paramProb;
+    float         m_profileProb;
+    float         m_tkrEnergyFrac;
+    float         m_tkrLATEdge;
+    float         m_trackerProb;
+    float         m_VTX;
 
     int           m_executeTreeCnt;
     int           m_goodVals;
