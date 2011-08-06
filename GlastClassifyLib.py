c9c2e620b2f174475b8317166e4b7b4dd100e165
heather <>
2011-08-06 05:14:35 +0000

    merge in SCons updates along branch

--- GlastClassifyLib.py
+++ GlastClassifyLib.py
@@ -1,10 +1,14 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GlastClassify/GlastClassifyLib.py,v 1.1 2008/08/15 21:42:35 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastClassify/GlastClassifyLib.py,v 1.5 2011/03/08 00:10:23 jrb Exp $
 def generate(env, **kw):
-    if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = ['GlastClassify'])
-    env.Tool('facilitiesLib')
+    if not kw.get('noGaudi', 0):
+        env.Tool('addLibrary', library = env['gaudiLibs'])
+        if not kw.get('depsOnly', 0):
+            env.Tool('addLibrary', library = ['GlastClassify'])
     env.Tool('classifierLib')
     env.Tool('ntupleWriterSvcLib')
     env.Tool('xmlBaseLib')
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['xercesLibs'])
+    env.Tool('addLibrary', library = env['TMineLibs'])
 def exists(env):
     return 1;
