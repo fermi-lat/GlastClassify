044e9aa192668eed789267f5e8977e54713652ba
usher <>
2011-08-17 19:25:26 +0000

    Need this to expose our ClassifyTool

--- src/Dll/GlastClassify.cxx
+++ src/Dll/GlastClassify.cxx
@@ -3,13 +3,14 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/merit/src/Dll/merit_load.cxx,v 1.5 2005/11/08 15:47:38 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/GlastClassify/src/Dll/GlastClassify.cxx,v 1.1 2006/10/23 21:32:19 usher Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(GlastClassify) {
-    DECLARE_ALGORITHM( ClassifyAlg);
+    DECLARE_ALGORITHM(ClassifyAlg);
+    DECLARE_TOOL(ClassifyTool);
 } 
 
 
