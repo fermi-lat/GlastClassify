044d5291076ed5b2df6ea136e6669f6132bbb106
heather <>
2012-08-15 05:06:12 +0000

    remove ClassificationTree static lib

--- GlastClassifyLib.py
+++ GlastClassifyLib.py
@@ -1,9 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GlastClassify/GlastClassifyLib.py,v 1.4 2009/11/21 00:35:19 jrb Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastClassify/GlastClassifyLib.py,v 1.5 2011/03/08 00:10:23 jrb Exp $
 def generate(env, **kw):
-    if not kw.get('noGaudi', 0):
-        env.Tool('addLibrary', library = env['gaudiLibs'])
-        if not kw.get('depsOnly', 0):
-            env.Tool('addLibrary', library = ['GlastClassify'])
+#    if not kw.get('noGaudi', 0):
+#        env.Tool('addLibrary', library = env['gaudiLibs'])
+    if not kw.get('depsOnly', 0):
+        env.Tool('addLibrary', library = ['GlastClassify'])
     env.Tool('classifierLib')
     env.Tool('ntupleWriterSvcLib')
     env.Tool('xmlBaseLib')
